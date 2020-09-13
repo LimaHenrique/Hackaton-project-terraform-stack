0e633cf8c9070c48fb851044c7f62320fc8e9dcb
LimaHenrique <henrique_margarido@hotmail.com>
2020-09-13 17:34:48 -0300

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-2dvp-335553"
+    bucket = "hackaton-fiap-2dvp-335553"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
