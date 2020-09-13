d08dcbdc2f24000db4164916f75b69969ce699f8
LimaHenrique <henrique_margarido@hotmail.com>
2020-09-13 17:08:11 -0300

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cd-homolog-2dvp-335553"
+    bucket = "hackathon-fiap-2dvp-335553"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
